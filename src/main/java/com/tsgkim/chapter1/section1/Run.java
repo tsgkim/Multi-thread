e1e426f2e8fe7e2af5aed1462624bce75fe66055
shiguang.tu <shiguang.tu@andpay.me>
2017-10-19 10:00:15 +0800

    add code

--- src/main/java/com/tsgkim/chapter1/section1/Run.java
+++ src/main/java/com/tsgkim/chapter1/section1/Run.java
@@ -3,9 +3,8 @@ package com.tsgkim.chapter1.section1;
 import org.junit.Test;
 
 /**
- *
  * Study:
- *      使用多线程技术时，代码运行结果与代码执行顺序或者调用顺序无关
+ * 使用多线程技术时，代码运行结果与代码执行顺序或者调用顺序无关
  *
  * @Description:
  * @author: shiguang.tu
@@ -15,9 +14,15 @@ public class Run {
 
     @Test
     // 有可能先打印 运行结束  再打印 MyThread
-    // 如果多次调用 start() 会抛出异常  Exception in thread "main" java.lang.IllegalThreadStateException
+
     public void myRun() {
         MyThread myThread = new MyThread();
+
+        // 如果多次调用 start() 会抛出异常  Exception in thread "main" java.lang.IllegalThreadStateException
+//        for (int i=0; i<20;i++){
+//            myThread.start();
+//        }
+
         myThread.start();
         System.out.println("运行结束");
     }
