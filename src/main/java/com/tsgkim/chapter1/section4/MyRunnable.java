b1fde886ed970137792cbb51927e5f9090ef89f9
shiguang.tu <shiguang.tu@andpay.me>
2017-10-21 00:21:05 +0800

    Runnable 学习

---
+++ src/main/java/com/tsgkim/chapter1/section4/MyRunnable.java
@@ -0,0 +1,17 @@
+package com.tsgkim.chapter1.section4;
+
+/**
+ * Study:
+ *
+ * @Description:
+ * @author: shiguang.tu
+ * @create: 17/10/20 下午11:55
+ **/
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("运行中！");
+    }
+}
+
---
+++ src/main/java/com/tsgkim/chapter1/section4/Run.java
@@ -0,0 +1,32 @@
+package com.tsgkim.chapter1.section4;
+
+import com.tsgkim.chapter1.section1.MyThread;
+import org.junit.Test;
+
+/**
+ * Study:
+ *
+ * @Description:
+ * @author: shiguang.tu
+ * @create: 17/10/21 上午12:11
+ **/
+public class Run {
+
+    @Test
+    /*
+        Thread implements Runnable
+        Thread 构造方法中 可传入 Runnable 对象
+        ==》可以将一个 Thread 对象中的 run() 方法交由其它线程进行调用
+     */
+    public void myTest() {
+        Runnable runnable = new MyRunnable();
+        Thread thread1 = new Thread(runnable);
+        thread1.start();
+
+        Thread thread2 = new MyThread();
+        Thread thread3 = new Thread(thread2);
+        thread3.start();
+
+        System.out.println("运行结束！");
+    }
+}
