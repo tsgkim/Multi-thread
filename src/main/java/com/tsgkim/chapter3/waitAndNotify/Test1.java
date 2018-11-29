95faab1960bc166de7f64b7bad1027241a857c48
shiguang.tu <shiguang.tu@andpay.me>
2018-11-29 10:12:49 +0800

    waitAndNotify使用

--- src/main/java/com/tsgkim/chapter3/waitAndNotify/Test1.java
+++ src/main/java/com/tsgkim/chapter3/waitAndNotify/Test1.java
@@ -3,7 +3,7 @@ package com.tsgkim.chapter3.waitAndNotify;
 /**
  * 
  * @author: shiguang.tu
- * @create: 2018/11/27 5:27 PM
+ * @create: 2018/11/27 5:28 PM
  */
 public class Test1 {
 
@@ -12,10 +12,26 @@ public class Test1 {
      */
     public static void main(String[] args) throws InterruptedException {
 
-        String ss = null;
+        String ss = "ss";
         ss.wait();
 
     }
 
+}
+
+class Test_1 {
+
+    /**
+     * 必须对 wait(); 对应的对象加锁，对应的对象才能使用 wait()，不是对应的对象加锁还是报异常
+     */
+    public static void main(String[] args) throws InterruptedException {
+
+        String ss = "ss";
+
+        synchronized (ss) {
+            ss.wait();
+        }
+
+    }
 
 }
